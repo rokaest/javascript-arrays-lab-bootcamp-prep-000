ce805c6eda92092474c825165040098716d13c32
Ro Kaest <rvnadreau@ucdavis.edu>
2018-09-25 21:46:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  var kitten = kittens.push(name)
+  return kitten
+}
+
+function destructivelyPrependKitten(name) {
+  var kitten = kittens.unshift(name)
+  return kitten
+}
+
+function destructivelyRemoveLastKitten() {
+  var kitten = kittens.pop()
+  return kitten
+}
+
+function destructivelyRemoveFirstKitten() {
+  var kitten = kittens.shift()
+  return kitten
+}
+
+function appendKitten(name) {
+  var newKittens = [...kittens, name]
+  return newKittens
+}
+
+function prependKitten(name) {
+  var newKittens = [name, ...kittens]
+  return newKittens
+}
+
+function removeLastKitten() {
+  var newKittens = kittens.splice(-1)
+  return newKittens
+}
+
+function removeFirstKitten() {
+  var newKittens = kittens.slice(0)
+  return newKittens
+}
+
+
+
